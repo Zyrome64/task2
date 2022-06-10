37544af6bb895c740cfcdb7e57bbb880cd52b4dc
aivanov <aivanov@mit.cloudvps.regruhosting.ru>
2022-06-10 09:43:47 +0300

    added fib function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+int fib(int n){
+    if(n == 1  || n == 2){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
 int main(){
-    cout << "Hello, World!";
+    cout << fib(10);
 }
